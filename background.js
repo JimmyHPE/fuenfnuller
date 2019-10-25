b8618742f25ae6e1e62280ae5a7d2d2e8c58f04b
Jablonski <jimmy.jablonski@hpe.com>
2019-10-25 22:22:20 +0200

    hintergrund dazu

---
+++ background.js
@@ -0,0 +1,14 @@
+
+function befuelleZeile(){
+  for (let i = 0; i < 200; i++) {
+      let test = Math.floor((Math.random() * 2));
+
+      if (test == 1) {
+        var emoji = "&#127867";
+      }
+      else {
+        var emoji = "&#127866";
+      }
+      document.writeln(emoji);
+  }
+}
