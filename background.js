29d12a669c8c96e72b42d682161861149656f85f
neinson <56021833+neinson@users.noreply.github.com>
2019-10-28 12:26:13 +0100

    meine änderungen

--- background.js
+++ background.js
@@ -1,10 +1,19 @@
 
 function befuelleZeile(){
   for (let i = 0; i < 95; i++) {
-      let test = Math.floor((Math.random() * 2));
+      let test = Math.floor((Math.random() * 5));
 
-      if (test == 1) {
+      if (test == 0) {
         var emoji = "&#127867";
+      }
+      else if (test == 1) {
+        var emoji = "&#127865";
+      }
+      else if (test == 2) {
+        var emoji = "&#127864";
+      }
+      else if (test == 3) {
+        var emoji = "&#127863";
       }
       else {
         var emoji = "&#127866";
@@ -12,3 +21,4 @@ function befuelleZeile(){
       document.writeln(emoji);
   }
 }
+
